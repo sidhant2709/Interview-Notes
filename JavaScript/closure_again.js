722f2bb81bf5cadb8ebc3d54b75a4dc940b10b1c
Sidhant <sidhant1593@gmail.com>
2024-05-23 03:26:10 +0530

    closure example

--- JavaScript/closure_again.js
+++ JavaScript/closure_again.js
@@ -1,21 +1,23 @@
-var n = 2;
+'use strict';
 
-function square(num) {
-  var ans = num * num;
-  return ans;
-}
+// var n = 2;
 
-var squareNum = square(n);
-var squareNum2 = square(3);
+// function square(num) {
+//   var ans = num * num;
+//   return ans;
+// }
 
-const obj = {
-  n: undefined,
-  square: square,
-  squareNum: undefined,
-  squareNum2: undefined,
-};
+// var squareNum = square(n);
+// var squareNum2 = square(3);
 
-console.log(obj);
+// const obj = {
+//   n: undefined,
+//   square: square,
+//   squareNum: undefined,
+//   squareNum2: undefined,
+// };
+
+// console.log(obj);
 
 // function outerFunction() {
 //   let outerVariable = 'I am an outer variable';
@@ -30,3 +32,43 @@ console.log(obj);
 // const myClosure = outerFunction();
 
 // console.log(myClosure);
+
+/*
+  Write a function such that is execution is as follows:
+  computeAmount().lacs(15).crore(5).crore(2).lacs(20).thousand(45).crore(7).value()
+
+  Output: 143545000
+*/
+
+function computeAmount() {
+  let totalSum = 0;
+
+  return {
+    lacs: function (num) {
+      totalSum += num * 100000;
+      return this;
+    },
+    crore: function (num) {
+      totalSum += num * 10000000;
+      return this;
+    },
+    thousand: function (num) {
+      totalSum += num * 1000;
+      return this;
+    },
+    value: function () {
+      return totalSum;
+    },
+  };
+}
+
+let ans = computeAmount()
+  .lacs(15)
+  .crore(5)
+  .crore(2)
+  .lacs(20)
+  .thousand(45)
+  .crore(7)
+  .value();
+
+console.log(ans);
